70921a72411e883be4fd5d4e7050b696fa7d5d6f
hninnuyeemonhan-coder <w25043192@northumbria.ac.uk>
2026-05-02 19:16:06 +0100

    Fix basename for deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/w25043192/pawfriend/app/'>
+    <BrowserRouter basename='/pawfriend/app/'>
       <AuthProvider>
         <App />
       </AuthProvider>
